4690c6eeae758dae89f68c501c80f44183dea7e7
Adam Martinez <adam70181@yahoo.com>
2015-02-08 14:09:03 -0600

    initial commit.

---
+++ src/multtable.php
@@ -0,0 +1,12 @@
+<?php
+	$min_multiplicand = $_REQUEST['min_multiplicand'];
+	$max_multiplicand = $_REQUEST['max_multiplicand'];
+	$min_multiplier = $_REQUEST['min_multiplier'];
+	$max_multiplier = $_REQUEST['max_multiplier'];
+	
+	echo("min_multiplicand: " . $min_multiplicand);
+	echo("<br/>max_multiplicand: ". $max_multiplicand);
+	echo("<br/>min_multiplier " . $min_multiplier);
+	echo("<br/>max_multiplier " . $max_multiplier);
+?>
+
