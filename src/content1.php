99d094a3f649173b9e5ba327a7a619da1753ed28
Adam Martinez <adam70181@yahoo.com>
2015-02-08 21:29:24 -0600

    Initial commit. Have username

---
+++ src/content1.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+?>
+
+<html>
+<head>
+<title>content1</title>
+</head>
+<body>
+
+<?php
+	if(!isset($_SESSION['visits'])) {
+		$_SESSION['visits'] = 0;
+	}
+	
+	$username = $_POST['username'];
+	
+	if(strlen($username) > 0) {
+		echo ("Hello " . $username . "you have visited this page " . $_SESSION['visits'] . " times before.");
+		$_SESSION['visits']++;
+	} else {
+		echo ("A username must be entered.<br/>");
+		echo ("Click ");
+		echo '<a href="login.php">here</a>';
+		echo (" to return to the login screen.");
+	}
+	
+?>
+</table>
+</body>
+</html>
