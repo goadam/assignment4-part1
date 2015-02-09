bc1571d1cc045799315c37a783101b3f5bb9ee44
Adam Martinez <adam70181@yahoo.com>
2015-02-08 22:33:12 -0600

    turn in

--- src/content1.php
+++ src/content1.php
@@ -12,13 +12,17 @@ session_start();
 <?php
 	if(!isset($_SESSION['visits'])) {
 		$_SESSION['visits'] = 0;
+		$_SESSION['l'] = 0;
 	}
 	
 	$username = $_POST['username'];
 	
 	if(strlen($username) > 0) {
-		echo ("Hello " . $username . "you have visited this page " . $_SESSION['visits'] . " times before.");
+		echo ("Hello " . $username . " you have visited this page " . $_SESSION['visits'] . " times before.");
 		$_SESSION['visits']++;
+		echo ("<br/>Click ");
+		echo '<a href ="log.php">here</a>'; 
+		echo(" to logout.");
 	} else {
 		echo ("A username must be entered.<br/>");
 		echo ("Click ");
